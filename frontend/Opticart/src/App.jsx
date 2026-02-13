782597468742a0df5231a0b03163afeea049f949
ninjangelo <lagdameoangelo@gmail.com>
2026-02-13 02:11:05 +0000

    Simple landing page

--- frontend/Opticart/src/App.jsx
+++ frontend/Opticart/src/App.jsx
@@ -15,7 +15,11 @@ function App() {
   return (
     <>
       <HomeNavbar />
-      <h1 className="text-2xl font-bold underline text-blue-600 text-center p-78">TailwindCSS</h1>
+      <div className="flex flex-col px-30 pt-25 pb-54 text-center items-center justify-center space-y-4">
+        <img className="w-45" src="/temporary_logo.svg" alt="opticart_logo"/>
+        <h1 className="text-8xl text-temporary-turqoise font-montserrat">Opticart</h1>
+        <p className="text-2xl text-temporary-turqoise2 font-manrope">Budget-friendly Meal Planning made easy and effective</p>
+      </div>
     </>
   )
 }
