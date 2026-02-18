75b590bbd21d2a7e8023f3fee5567450d9fc52eb
ninjangelo <lagdameoangelo@gmail.com>
2026-02-18 17:12:30 +0000

    included scrollReset function in home_footer.jsx

--- frontend/Opticart/src/components/home_footer.jsx
+++ frontend/Opticart/src/components/home_footer.jsx
@@ -2,9 +2,16 @@ import { Link } from "react-router-dom";
 
 function HomeFooter() {
 
+    function scrollReset() {
+        window.scrollTo({
+            top: 0,
+            behavior: "auto"
+        });
+    };
+
     return (
         <footer className="flex flex-col bg-temporary-turqoise text-white py-12">
-            <div className="font-montserrat text-4xl pb-7 border-white border-b-2 mx-22">
+            <div className="font-montserrat text-4xl pb-7 border-white border-b-2 mx-22" onClick={scrollReset}>
                 <Link to="/" className="flex flex-col items-center justify-center space-y-4">
                     <img className="w-18" src="/temporary_logo.svg" alt="opticart_logo"/>
                     <h1>Opticart</h1>
